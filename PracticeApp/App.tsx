c9d64fd9ca2e9c79ea6737bedfe14b8ab2c4f357
MaxiARG <maxiramos2017@gmail.com>
2022-12-29 18:50:32 -0300

    agrego .env

--- PracticeApp/App.tsx
+++ PracticeApp/App.tsx
@@ -2,19 +2,27 @@
 /* eslint-disable semi */
 /* eslint-disable react/self-closing-comp */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Text, View} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import Config from 'react-native-config';
 
 const Drawer = createDrawerNavigator();
 
 function HomeScreen() {
+
+useEffect(() => {
+  console.log(Config.URL_API)
+
+})
+
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
+      <Text>Home Screen {Config.URL_API} </Text>
+    </View> 
   );
 }
 
